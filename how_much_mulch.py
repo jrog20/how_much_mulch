f3824a6f186bb1d1765ca97a70249061648f677b
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-05-02 18:06:41 -0400

    Update how_much_mulch.py

--- how_much_mulch.py
+++ how_much_mulch.py
@@ -2,6 +2,9 @@
 File: how_much_mulch.py
 ------------------
 This program calculates the amount of mulch needed for garden beds.
+The user is prompted to give length and width of all garden beds.
+The program then calculates how much mulch will be needed to cover all
+garden beds at a constant depth.
 """
 
 MULCH_DEPTH = 2
@@ -9,49 +12,31 @@ INCHES_IN_FEET = 12
 CUFEET_IN_CUYARD = 27
 
 def main():
-    """
-    User is prompted to give length and width of all garden beds on property.
-    The program then calculates how much mulch will be needed to cover all
-    garden beds at a constant depth.
-    """
-
-    # tell the user what this program does.
-    print("This program calculates the amount of mulch you will need for your garden beds.")
 
     bed_num = 0
+    total_beds_volume_cuft = 0
+    total_beds_volume_cuyd = 0
 
-    # count the garden bed numbers (this is last, before the while loop continues for the next bed.)
-    bed_num += 1
+    print("This program calculates the amount of mulch you will need for your garden beds.")
 
-    # loop calculate area for all beds entered by user
     while input != 0:
-        # ask user to input length of garden bed (in feet).
+        bed_num += 1
         length_ft = float(input("Enter the length of garden bed number " + str(bed_num) + " in feet: "))
-        # ask user to input width of garden bed (in feet).
         width_ft = float(input("Enter the width of garden bed number " + str(bed_num) + " in feet: "))
 
-        # calculate_area of bed in square feet
         area_bed_sqft = length_ft * width_ft
 
-        # calculate_volume of mulch needed for a bed
         volume_bed_cuft = area_bed_sqft * (MULCH_DEPTH / INCHES_IN_FEET)
         volume_bed_cuyd = area_bed_sqft * (MULCH_DEPTH / INCHES_IN_FEET) / CUFEET_IN_CUYARD
 
-        # output to user in cubic feet
-        print("The cubic feet of mulch needed for garden bed number " + str(bed_num) + " is: " + str(volume_bed_cuft))
+        total_beds_volume_cuft += volume_bed_cuft
+        total_beds_volume_cuyd += volume_bed_cuyd
 
-        # output to user in cubic yards
-        print("The cubic yards of mulch needed for garden bed number " + str(bed_num) + " is: " + str(volume_bed_cuyd))
+        print("The cubic feet of mulch needed for garden bed number", bed_num, "is:", volume_bed_cuft)
+        print("The cubic yards of mulch needed for garden bed number", bed_num, "is:", volume_bed_cuyd)
 
-        # add all volume_bed_cuft together
-
-
+        print("Total mulch needed for all garden beds is:", total_beds_volume_cuft, "cubic feet, or", total_beds_volume_cuyd, "cubic yards.")
 
 
-
-
-
-    # This provided line is required at the end of a Python file
-# to call the main() function.
 if __name__ == '__main__':
     main()
